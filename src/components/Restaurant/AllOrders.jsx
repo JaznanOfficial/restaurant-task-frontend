10c5fc3adc3adef6e32cdad6f0e840e916ba590e
JaznanOfficial <jaznanofficial@gmail.com>
2023-06-10 21:52:52 +0600

    all order page done

--- src/components/Restaurant/AllOrders.jsx
+++ src/components/Restaurant/AllOrders.jsx
@@ -1,88 +1,79 @@
 import { Col, Row } from "reactstrap";
-
-import '../../styles/cart-page.css'
-// import './AllOrders.css'
+import "../../styles/cart-page.css";
+import "./AllOrders.css";
 
 const AllOrders = () => {
     return (
-        <div className="my-5">
-            <div className="container d-flex justify-content-center align-items-center mt-50 mb-50">
-                <div className="row">
-                    <div className="col-md-12 w-100">
-                        <div className="card card-body">
-                            <div className="w-100 media align-items-center align-items-lg-start text-center text-lg-left d-flex flex-column flex-lg-row">
-                                <div className="mr-2 mb-3 mb-lg-0 ">
-                                    <img
-                                        src="https://i.imgur.com/5Aqgz7o.jpg"
-                                        width="150"
-                                        height="150"
-                                        alt=""
-                                        className="rounded-circle"
-                                    />
-                                </div>
+        <div>
+            <div className="container mt-5 mb-5">
+                <div className="d-flex justify-content-center row">
+                    <div className="col-md-12">
+                        <div className="row p-2 bg-white border rounded">
+                            <div className="col-md-3 mt-1">
+                                <img
+                                    className="img-fluid img-responsive rounded product-image "
+                                    src="https://i.imgur.com/QpjAiHq.jpg"
+                                />
+                            </div>
+                            <div className="col-md-6 mt-1">
+                                <h3 className="text-center text-md-start">Md Abdur Rahman</h3>
 
-                                <div className="media-body text-center text-md-start mx-3 p-3">
-                                    <h6 className="media-title font-weight-semibold">
-                                        <h1 >Md Abdur rahman</h1>
-                                    </h6>
-
-                                    <div className="mb-3 mb-lg-2 w-100">
-                                        <div>
-                                            <address className="text-muted w-100 address" data-abc="true">
-                                                ABCD Road, Eiffel Tower, 25th Floor Khulna-9000
-                                            </address>
-                                        </div>
-                                    </div>
-
-                                    {<Row>
-                                        <Col>
-                                            
+                                <p className="text-justify text-truncate para mb-0">
+                                    There are many variations of passages of Lorem Ipsum available,
+                                    <br />
+                                    <br />
+                                </p>
+                                {
+                                    <Row>
+                                        <Col className="w-100">
                                             <table className="table table-bordered">
                                                 <thead>
                                                     <tr>
-                                                        <th>Image</th>
-                                                        <th>Product Title</th>
-                                                        <th>Price</th>
-                                                        <th>Quantity</th>
-                                                        
+                                                        <th className="text-center">Image</th>
+                                                        <th className="text-center">
+                                                            Product Title
+                                                        </th>
+                                                        <th className="text-center">Price</th>
+                                                        <th className="text-center">Quantity</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     <tr>
                                                         <td className="text-center cart__img-box">
-                                                            <img src="https://i.imgur.com/5Aqgz7o.jpg" alt="" />
+                                                            <img
+                                                                src="https://i.imgur.com/5Aqgz7o.jpg"
+                                                                alt=""
+                                                                className="order_image"
+                                                            />
                                                         </td>
-                                                        <td className="text-center">{'Double Cheese Margherita'}</td>
-                                                        <td className="text-center">${'price'}</td>
                                                         <td className="text-center">
-                                                            {'quantity'}px
+                                                            {"Double Cheese Margherita"}
                                                         </td>
-                                                        
+                                                        <td className="text-center">${"120"}</td>
+                                                        <td className="text-center">{"3"}</td>
                                                     </tr>
                                                 </tbody>
                                             </table>
-                                            
                                         </Col>
-                                    </Row>}
-                                </div>
+                                    </Row>
+                                }
+                            </div>
+                            <div className="align-items-center align-content-center col-md-3 border-left mt-1">
+                                <h4 className="badge text-bg-info text-white fs-6 text-center text-md-start">
+                                    Pending...
+                                </h4>
 
-                                <div className="mt-3 mt-lg-0 ml-lg-3 text-center">
-                                    <h3 className="mb-0 font-weight-semibold">$459.99</h3>
+                                <h2 className="mr-1 text-center text-md-start">$13.99</h2>
 
-                                    <div>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                    </div>
-
-                                    <div className="text-muted">1985 reviews</div>
-
-                                    <button
-                                        type="button"
-                                        className="btn btn-warning mt-4 text-white"
-                                    >
-                                        <i className="icon-cart-add mr-2"></i> Add to cart
+                                <h6 className="text-success text-center text-md-start">
+                                    Free shipping
+                                </h6>
+                                <div className="d-flex flex-column mt-4 text-center text-md-start">
+                                    <button className="btn btn-success btn-sm" type="button">
+                                        Accept
+                                    </button>
+                                    <button className="addTOCart__btn btn-sm mt-2" type="button">
+                                        Reject
                                     </button>
                                 </div>
                             </div>
